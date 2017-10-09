9f1536ad040ffe76b3c71d26581b07abed7760c5
Ventresco-Heather <31718475+Ventresco-Heather@users.noreply.github.com>
2017-10-08 21:47:37 -0700

    Add files via upload

---
+++ GuessNumber/src/Practice.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int user, computer;
+		
+		computer = 1 + (int) ( Math.random() * 10 );  
+		System.out.print("The computer chose ");
+		System.out.println(computer);  //random number generated
+		
+		System.out.println("Guess A Number 1 to 10:");
+		user = input.nextInt();
+		
+		
+		
+		
+		
+	}
+
+}
+
+
